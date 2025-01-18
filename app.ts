bea25a54bde745d70aca246b7a049797bba81064
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-01-18 18:52:38 +0530

    disable strict matching for routes to handle trailing slashes

--- app.ts
+++ app.ts
@@ -2,7 +2,11 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import expenseRoute from "./routes/expenseRoute";
 
-const app = new Hono();
+const app = new Hono({strict:false});
+/*
+Disabled strict matching for routes by setting `strict: false` to handle both with and without trailing slashes.
+This ensures `/api/expense` and `/api/expense/` are treated as the same route.
+*/
 
 app.use("*", logger());
 app.route("/api/expense",expenseRoute)
