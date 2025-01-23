2a672d3b66251090991e6dfb3353bb7d6357f369
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-01-24 03:24:12 +0530

    update import path for ApiRoutes to use aliasing in Vite config

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 // import { type ApiRoutes } from "../../../server/app"; // added @server path into tsConfig.json and viteConfig.json
-import { type ApiRoutes } from "@server/app";
+import { type ApiRoutes } from "@/server/app";
 import { hc } from "hono/client";
 
 const client = hc<ApiRoutes>("/");
